9687570f8614b2e6970a516eef49abf3280fbf4e
ICSE2022 <jcaoap@connect.ust.hk>
2021-09-07 20:35:05 +0800

    add code book

---
+++ CodeBook/Analyzer/lineLoc.py
@@ -0,0 +1,36 @@
+import sys
+from CodeBook.Analyzer.ASTParser import *
+import os
+import ast
+import numpy as np
+
+sys.path.append("./")
+
+
+def lineLoc(pred_voting, subject_name):
+    # input predicted label, subject names
+    # shape of pred: [5], shape of subject_name: str
+    # label_name = "lb_opt, lb_lr, lb_act, lb_loss, lb_epoch"
+    label_name = ['optimizer', 'lr', 'act', 'loss', 'epoch']
+    print("working on subject: ", subject_name)
+    base_dir = "./Evaluations/Subjects"
+    subject_dir = os.path.join(base_dir, subject_name)
+    analyzer = Analyzer()
+    for mutant_dir in os.listdir(subject_dir):
+        if os.path.isfile(os.path.join(subject_dir, mutant_dir)):
+            continue
+        content = open(os.path.join(subject_dir, mutant_dir, "origin.py")).read()
+        tree = ast.parse(content)
+        try:
+            analyzer.visit(tree)
+            line_no = analyzer.lineno  # line_no: dict["optimizer":[x,x], "loss":[x,x], ...]
+            buggy_label = np.where(pred_voting == 1)[0]
+            print("Fault diagnosis and localization powered by DeepFD:")
+            for lid, label_index in enumerate(buggy_label, 1):
+                name = label_name[label_index]
+
+                print("Fault {}: [{}], Line numbers: [{}]".format(lid, name, line_no[name]))
+
+        except Exception as e:
+            print(e)
+            continue
